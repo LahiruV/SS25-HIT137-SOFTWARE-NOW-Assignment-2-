12c1c5c8fc27d774eadf22ab0ae6b995b419a432
Lahiru Vimukthi <lahiruvimukthi292@gmail.com>
2026-01-08 23:02:16 +0930

    Fix import statement for writers module in main.py

--- S397804 - Q2/main.py
+++ S397804 - Q2/main.py
@@ -1,5 +1,6 @@
 from data_loader import load_all_files
 from analysis import seasonal_averages, station_ranges, station_stability
+from writers import write_average_file, write_range_file, write_stability_file
 
 def main() :
       data = load_all_files()
@@ -7,6 +8,9 @@ def main() :
       ranges = station_ranges(data)
       stability = station_stability(data)
       
+      write_average_file(averages)
+      write_range_file(ranges)
+      write_stability_file(stability)
 
 if __name__ == "__main__":
     main()
