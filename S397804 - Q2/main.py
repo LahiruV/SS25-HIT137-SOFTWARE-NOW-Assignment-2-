83a90b0390ca93cc5efdd1430efeed85bd00c250
Lahiru Vimukthi <lahiruvimukthi292@gmail.com>
2026-01-08 22:58:01 +0930

    Add function calls to compute seasonal averages, station ranges, and stability in main

--- S397804 - Q2/main.py
+++ S397804 - Q2/main.py
@@ -1,7 +1,12 @@
 from data_loader import load_all_files
+from analysis import seasonal_averages, station_ranges, station_stability
 
 def main() :
       data = load_all_files()
+      averages = seasonal_averages(data)
+      ranges = station_ranges(data)
+      stability = station_stability(data)
+      
 
 if __name__ == "__main__":
     main()
