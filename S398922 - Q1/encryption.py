888cf809de9889b22bb10508f0f08341c668ec42
syedshoaib-050 <syedshoaib819045@gmail.com>
2026-01-12 11:45:51 +0530

    Add initial encryption module and raw text file

---
+++ S398922 - Q1/encryption.py
@@ -0,0 +1,6 @@
+import os
+
+# -------- FILE PATHS --------
+RAW_FILE = r"C:/Users/syeds/Downloads/SN GIT/SS25-HIT137-SOFTWARE-NOW-Assignment-2-/S398922 - Q1/raw_text.txt"
+ENCRYPTED_FILE = "encrypted_text.txt"
+DECRYPTED_FILE = "decrypted_text.txt"
