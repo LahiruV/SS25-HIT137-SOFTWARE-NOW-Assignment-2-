a56503c35fe8160fe75959d4a5d4b03dc14c3618
Afaan Shaik <shaikafaan048@gmail.com>
2026-01-12 13:41:02 +0530

    Refactor inward_koch function for improved readability and structure

--- S398776 - Q3/problem.py
+++ S398776 - Q3/problem.py
@@ -5,3 +5,16 @@ import math
 def inward_koch(t, length, depth):
     if depth == 0:
         t.forward(length)
+    else:
+        length /= 3
+
+        inward_koch(t, length, depth - 1)
+
+        t.right(60)
+        inward_koch(t, length, depth - 1)
+
+        t.left(120)
+        inward_koch(t, length, depth - 1)
+
+        t.right(60)
+        inward_koch(t, length, depth - 1)
